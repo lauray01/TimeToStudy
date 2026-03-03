974b98e5dacb75e04174fdc7bc2be1ed05f4c097
Laura Ayuso <lauray01@ucm.es>
2026-03-03 22:00:24 +0100

    Buttons organized, now theres a selectables timer

--- app/src/main/java/com/example/timetostudy/FirstFragment.java
+++ app/src/main/java/com/example/timetostudy/FirstFragment.java
@@ -1,10 +1,12 @@
 package com.example.timetostudy;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,6 +18,9 @@ import java.text.NumberFormat;
 
 public class FirstFragment extends Fragment{
     private TextView textView;
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis = 1500000;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -29,21 +34,60 @@ public class FirstFragment extends Fragment{
         super.onViewCreated(view, savedInstanceState);
 
         textView = view.findViewById(R.id.timer);
+        Button btn25 = view.findViewById(R.id.twentyFiveMin);
+        Button btn10 = view.findViewById(R.id.tenMin);
+        Button btn5 = view.findViewById(R.id.fiveMin);
+        Button btnStart = view.findViewById(R.id.buttonStart);
 
-        new CountDownTimer(1500000, 1000){
+        btn25.setOnClickListener( v -> {
+            updateTimeSelection(1500000);
+        });
 
+        btn10.setOnClickListener( v -> {
+            updateTimeSelection(600000);
+        });
+
+        btn5.setOnClickListener( v -> {
+            updateTimeSelection(300000);
+        });
+
+        btnStart.setOnClickListener( v -> {
+            startTimer(timeLeftInMillis);
+        });
+    }
+
+    private void updateTimeSelection(long millis){
+        if (countDownTimer != null) countDownTimer.cancel();
+        timeLeftInMillis = millis;
+        updateText(millis);
+    }
+
+    private void updateText(long millis){
+        NumberFormat f = new DecimalFormat("00");
+        long min = (millis / 60000) % 60;
+        long sec = (millis /1000) % 60;
+        textView.setText(f.format(min) + ":" + f.format(sec));
+    }
+    private void startTimer(long duration) {
+        if (countDownTimer != null) countDownTimer.cancel();
+        countDownTimer = new CountDownTimer(duration, 1000) {
+            @Override
             public void onTick(long millisUntilFinished) {
-                NumberFormat f = new DecimalFormat("00");
-                long seconds = (millisUntilFinished / 1000) % 60;
-                long minutes = (millisUntilFinished / 60000) % 60;
-                long hours = (millisUntilFinished / 360000) % 60;
-                String time = f.format(hours) + ":" + f.format(minutes) + ":" + f.format(seconds);
-                textView.setText(time);
+                timeLeftInMillis = millisUntilFinished;
+                updateText(millisUntilFinished);
             }
+            @Override
             public void onFinish() {
-                textView.setText("00:00");
+                showAlert();
             }
-
         }.start();
+    }
+    private void showAlert() {
+        new AlertDialog.Builder(getContext())
+                .setTitle("Time's up!")
+                .setMessage("Work session finished. Start a break?")
+                .setPositiveButton("Start Break (5mins)", (dialog, which) -> startTimer(300000))
+                .setNegativeButton("Dismiss", null)
+                .show();
     }
 }
